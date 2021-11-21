12945696c737d8e372427fd67af4b3d4d27ac171
vladyslav.kuchin <vladyslav.kuchin@tatemgames.com>
2021-11-21 22:21:24 +0200

    Initial commit

---
+++ Lab1/Registration/CredentialsJournalHolder.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab1.Registration
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class CredentialsJournalHolder
+    {
+        private const string CredentialsPath = "./Storage/System/credentials_journal.txt";
+        public List<UserRecord> Journal;
+
+        public CredentialsJournalHolder()
+        {
+            LoadJournalFromDisk();
+        }
+
+        private void LoadJournalFromDisk()
+        {
+            if (!File.Exists(CredentialsPath)) return;
+            
+            var credentialsList = File.ReadLines(CredentialsPath)
+                .Select(line => line.Split(","))
+                .Select(userInfo => new UserRecord
+                {
+                    Login = userInfo[0],
+                    Password = userInfo[1]
+                })
+                .ToList();
+
+            Journal = credentialsList;
+        }
+
+        public void SaveOnDisk()
+        {
+            var lines = Journal.Select(userInfo => $"{userInfo.Login},{userInfo.Password}");
+            File.WriteAllLines(CredentialsPath, lines);
+        }
+    }
+
+    public struct UserRecord
+    {
+        public string Login;
+        public string Password;
+    }
+}
---
+++ Lab1/Registration/UserRegistrar.cs
@@ -0,0 +1,30 @@
+﻿namespace Lab1.Registration
+{
+    using System.Collections.Generic;
+
+    public class UserRegistrar
+    {
+        public bool CreateUser(string login, string password)
+        {
+            var journalHolder = new CredentialsJournalHolder();
+            var isUserExist = CheckCollision(journalHolder.Journal, login);
+            if (isUserExist) return false;
+            journalHolder.Journal.Add(new UserRecord
+            {
+                Login = login,
+                Password = password
+            });
+            return true;
+        }
+
+        private bool CheckCollision(List<UserRecord> journal, string login)
+        {
+            foreach (var userRecord in journal)
+            {
+                if (userRecord.Login == login) return true;
+            }
+
+            return false;
+        }
+    }
+}
