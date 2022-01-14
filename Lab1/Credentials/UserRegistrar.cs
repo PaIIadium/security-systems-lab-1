929c4a51d35a96bfbb1457b192b3d2f19f1e217a
vladyslav.kuchin <vladyslav.kuchin@tatemgames.com>
2022-01-14 14:31:01 +0200

    Added password rules

--- Lab1/Credentials/UserRegistrar.cs
+++ Lab1/Credentials/UserRegistrar.cs
@@ -2,10 +2,50 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Text.RegularExpressions;
 
     public class UserRegistrar
     {
         private readonly CredentialsJournalHolder credentialsJournalHolder;
+
+        private const int PasswordMinLength = 8;
+
+        private static readonly List<PasswordRule> PasswordRules = new()
+        {
+            new PasswordRule
+            {
+                Regex = new Regex("[A-Z]"),
+                MatchesCount = 2,
+                Comment = "Password should contain at least 2 capital letters"
+            },
+            new PasswordRule
+            {
+                Regex = new Regex("[a-z]"),
+                MatchesCount = 2,
+                Comment = "Password should contain at least 2 lowercase letters"
+            },
+
+            new PasswordRule
+            {
+                Regex = new Regex("[0-9]"),
+                MatchesCount = 2,
+                Comment = "Password should contain at least 2 numbers"
+            },
+
+            new PasswordRule
+            {
+                Regex = new Regex("[^A-z0-9]"),
+                MatchesCount = 1,
+                Comment = "Password should contain at least 1 non-alphanumeric character"
+            },
+
+            new PasswordRule
+            {
+                Regex = new Regex(@"."),
+                MatchesCount = PasswordMinLength,
+                Comment = $"Password should contain at least {PasswordMinLength} characters"
+            }
+        };
 
         public UserRegistrar(CredentialsJournalHolder credentialsJournalHolder)
         {
@@ -20,11 +60,7 @@
                 return;
             }
 
-            if (!ValidatePassword(password))
-            {
-                Console.WriteLine("Password is invalid");
-                return;
-            }
+            if (!ValidatePassword(password)) return;
             
             credentialsJournalHolder.Journal.Add(new UserRecord
             {
@@ -47,7 +83,37 @@
 
         private bool ValidatePassword(string password)
         {
-            return password.Length >= 4;
+            if (CheckWhitespaces(password))
+            {
+                Console.WriteLine("Password should not contain any whitespace character");
+                return false;
+            }
+            return CheckPasswordRules(password);
         }
+
+        private bool CheckWhitespaces(string password)
+        {
+            return new Regex(@"\s").IsMatch(password);
+        }
+        
+        private bool CheckPasswordRules(string password)
+        {
+            foreach (var rule in PasswordRules)
+            {
+                if (rule.Regex.Matches(password).Count < rule.MatchesCount)
+                {
+                    Console.WriteLine(rule.Comment);
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public struct PasswordRule
+    {
+        public Regex Regex;
+        public int MatchesCount;
+        public string Comment;
     }
 }
